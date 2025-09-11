62c08ee56b114881f397684a9673114de63e0067
msvstj <savastijonok@gmail.com>
2025-09-11 17:41:51 +0300

    feat: increase gas limit for supported networks

--- apps/web/src/hooks/useGasLimit.ts
+++ apps/web/src/hooks/useGasLimit.ts
@@ -49,6 +49,8 @@ const GasMultipliers = {
   [chains.gno]: 1.3,
   [chains.zksync]: 20,
   [chains.sby]: 1.5,
+  [chains.sdn]: 1.5,
+  [chains.astr]: 1.3,
 }
 
 const incrementByGasMultiplier = (value: bigint, multiplier: number) => {
@@ -175,8 +177,8 @@ const useGasLimit = (
       .then((gasLimit) => {
         // Due to a bug in Nethermind estimation, we need to increment the gasLimit by 30%
         // when the safeTxGas is defined and not 0. Currently Nethermind is used only for Gnosis Chain.
-        if (currentChainId === chains.sby && hasSafeTxGas) {
-          return incrementByGasMultiplier(gasLimit, GasMultipliers[chains.sby])
+        if ([chains.sby, chains.astr, chains.sdn].includes(currentChainId) && hasSafeTxGas) {
+          return incrementByGasMultiplier(gasLimit, GasMultipliers[currentChainId])
         }
 
         return gasLimit
