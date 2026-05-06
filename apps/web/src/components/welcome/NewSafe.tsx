3b972e9de4497a3e40fb0b8d5a3d0cc45e63c535
Emosaddd <pl.maksim.krukovich@gmail.com>
2026-05-06 12:53:45 +0200

    feat: fix logo dimensions and hide spaces

--- apps/web/src/components/welcome/NewSafe.tsx
+++ apps/web/src/components/welcome/NewSafe.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Typography } from '@mui/material'
 import css from './styles.module.css'
 import WelcomeLogin from './WelcomeLogin'
-import SafeLabsLogo from '@/public/images/logo-safe-labs.svg'
+import SafeLogo from '@/public/images/logo.svg'
 import footerCss from './welcomeFooter.module.css'
 import Footer from '../common/Footer'
 
@@ -11,7 +11,7 @@ const NewSafe = () => {
     <div className={css.loginPage}>
       <div className={css.leftSide}>
         <div className={css.logoContainer}>
-          <SafeLabsLogo className={css.logo} />
+          <SafeLogo className={css.logo} />
         </div>
         <div className={css.loginContainer}>
           <WelcomeLogin />
